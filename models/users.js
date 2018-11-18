3f58dad48b333bee19603f62198df0a4a3c12f30
githubfisher <gooodfisher@126.com>
2018-11-19 07:51:09 +0800

    整理格式

--- models/users.js
+++ models/users.js
@@ -1,19 +1,19 @@
 // models/users.js
 
 module.exports = (sequelize, DataTypes) => sequelize.define(
-    'users',
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
+  'users',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-      tableName: 'users',
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-  );
+  },
+  {
+    tableName: 'users',
+  },
+);
